582bc947e15b51d5608e65e498cec0971ff19870
kimdohyon <kimdh102007@gmail.com>
2023-12-08 00:10:10 +0900

    Add files via upload

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -7,6 +7,9 @@ const ctrl = require('./home.ctrl');
 
 // ctrl 폴더에서 output이나 process 꺼내기
 router.get('/', ctrl.output.home);
-router.post('/list', ctrl.process.list)
 
+router.post('/list', ctrl.process.list)
+router.post('/edit', ctrl.process.edit)
+router.post('/image', ctrl.process.image)
+router.post("/users", ctrl.process.users)
 module.exports= router;
